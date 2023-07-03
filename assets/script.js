29c03f1d8595abd909b99785bea1052034e71fbc
lourdes-dorvilus <lourdesdorvilus@yahoo.com>
2023-07-03 16:31:00 -0400

    set up functionality to generate a password based on user input

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,45 @@
-// Assignment code here
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
+var numbers = "0123456789".split("");
+var special = "!@#$%^&*()_+-={}[]:".split("");
 
+// Assignment code here
+function generatePassword() {
+  var prePassword = [];
+  var postPassword = "";
+
+  var passwordLength = prompt(
+    "How long would you like your password to be? (8-128 characters)"
+  );
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Password must be between 8 and 128 characters. Please Retry!");
+    generatePassword();
+  } else {
+    if (confirm("Would you like to include uppercase letters?")) {
+      prePassword = prePassword.concat(upperCase);
+    }
+    if (confirm("Would you like to include lowercase letters?")) {
+      prePassword = prePassword.concat(lowerCase);
+    }
+    if (confirm("Would you like to include numbers?")) {
+      prePassword = prePassword.concat(numbers);
+    }
+    if (confirm("Would you like to include special characters?")) {
+      prePassword = prePassword.concat(special);
+    }
+    if (prePassword.length == 0) {
+      alert("You must choose at least one character type. Please Retry!");
+      generatePassword();
+    } else {
+      for (var i = 0; i < passwordLength; i++) {
+        postPassword =
+          postPassword +
+          prePassword[Math.floor(Math.random() * prePassword.length)];
+      }
+    }
+    return postPassword;
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,7 +50,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
